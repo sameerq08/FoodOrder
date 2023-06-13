55b008bfb8d3f7894f1eb93d0ece83fec08cf338
Sameer Quraishi <55457293+sameerq08@users.noreply.github.com>
2023-06-13 11:56:15 +0530

    Delete medicine_delivery directory

--- medicine_delivery/android/app/src/main/kotlin/com/example/medicine_delivery/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.medicine_delivery
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- medicine_delivery/lib/home_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-// ignore: use_key_in_widget_constructors
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Text('Hi');
-  }
-}
